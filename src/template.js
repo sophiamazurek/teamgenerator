ed86b58765f08aca7faa54c0280bdfd7a8ba1c2a
Sophia Mazurek <sophiamazurek@Sophias-MBP-2.lan>
2021-02-21 20:27:04 -0600

    added office number function to manager.js

--- src/template.js
+++ src/template.js
@@ -17,18 +17,36 @@ function engineerTemplate(employee){
     console.log(employee)
 //when I need to write the template having issues returning data to write to output.html
 console.log(employee.getName());
-console.log( `<div class="managercard card col mr-3 mb-3" style="width: 18rem;">
-                <div class="card-body">
-                  <h5 class="card-title">${employee.getName()}</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Role: ${employee.getRole()}</h6>
-                  <hr>
-                  <p class="card-text">ID: id</p><hr>
-                  <p class="card-text">Office Number: number</p>
-                  <hr>
-                  <a href="#" class="card-link">email</a>
-                </div>
-              </div>`)
+console.log( `<div class="engineercard card col mr-3 mb-3" style="width: 18rem;">
+                     <div class="card-body">
+                        <h5 class="card-title">${employee.getName()}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Role: ${employee.getRole()}</h6>
+                        <hr>
+                        <p class="card-text">ID: ${employee.getId()}</p><hr>
+                        <a href="#" class="card-link">${emplpyee.getGithub()}</a>
+                        <a href="#" class="card-link">${employee.getEmail()}</a>
+                        <hr>
+                    </div>
+                </div>`)
 
+}
+
+function managerTemplate(employee){
+    console.log("inside manager template");
+    console.log(employee)
+    //still having the same issue returning data. 
+    console.log(employee.getname());
+    console.log(`<div class="managercard card col mr-3 mb-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${employee.getName()}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Role: ${employee.getRole()}</h6>
+                        <hr>
+                        <p class="card-text">ID: ${employee.getId()}</p><hr>
+                        <p class="card-text">Office Number: ${employee.getOfficeNum()}</p>
+                        <hr>
+                        <a href="#" class="card-link">${employee.getEmail()}</a>
+                    </div>
+                </div>`)
 }
 
 
